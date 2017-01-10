51fefbde439317cd2d2e008004c91df023e15b07
Ashley Hsiao <aiyihsiao@gmail.com>
2017-01-09 16:51:42 -0800

    Add data validation to form

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -5,6 +5,9 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class PostType extends AbstractType
 {
@@ -13,9 +16,19 @@ class PostType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('body')->add('author')        ;
+        $builder->add('title', TextType::class, array(
+                    'required' => true
+                ))
+                ->add('body', TextareaType::class, array(
+                    'required' => true
+                ))
+                ->add('author', EntityType::class, array(
+                    'required' => true,
+                    'class' => 'AppBundle:User'
+                ))
+        ;
     }
-    
+
     /**
      * {@inheritdoc}
      */
